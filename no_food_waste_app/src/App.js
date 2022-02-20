48b101e89710729abe059f56af4b03b2edd08905
Niamat Zawad <zawad1879@gmail.com>
2022-02-19 18:40:56 -0600

    Add navbar

--- no_food_waste_app/src/App.js
+++ no_food_waste_app/src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <div>
+      <Header/>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- no_food_waste_app/src/components/Header.jsx
+++ no_food_waste_app/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 function header() {
   return (
-    <div>header</div>
+    <nav class="navbar navbar-light" style={{backgroundColor : "#4DFF8B"}}>
+      <a class="navbar-brand mx-2" href="#">Go Green!</a>
+      <form class="form-inline">
+        <a href="/login"><button class="btn btn-sm btn-outline-success mx-1" type="button">Login</button></a>
+        <button class="btn btn-sm btn-outline-success mx-2" type="button">Sign up</button>
+      </form>
+  </nav>
   )
 }
 
