81f7ae76080f251a7f1f15fed7652468cfdc972e
JoaquinAprendizaje <joaquinarturoaprendizaje@gmail.com>
2022-02-19 21:26:08 -0600

    [ADD] logic for activity

--- no_food_waste_app/src/components/Activity_form.jsx
+++ no_food_waste_app/src/components/Activity_form.jsx
@@ -1,168 +1,209 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { auth } from "../firebase";
+import { db } from  "../firebase";
+import { collection, addDoc, doc, updateDoc, arrayUnion } from "firebase/firestore";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Activity_form() {
+  const user = auth.currentUser;
+  const date = new Date();
+  toast.configure();
+  const [distanceWalked, setDistanceWalked] = useState(0);
+  const [distanceByVehicle, setDistanceByVehicle] = useState(0);
+  const [wasteProduced, setWasteProduced] = useState(0);
+  const [naturalLight, setNaturalLight] = useState(0);
+  const [showerTime, setShowerTime] = useState(0);
+  const [recycled, setRecycled] = useState(0);
+  const [diet, setDiet] = useState("neither");
+
+
+  //Function tu publish your activity
+  const publishActivity = async (e) => {
+    e.preventDefault();
+    
+    // Buscar el documento de assets que corresponde al usuario.
+    //const userRef = collection(db, "assets"); 
+    // Buscar el documento de usersdata que corresponde al usuario.
+    const usersdataRef = doc(db, "usersdata", user.uid);
+    //Se crea el archivo o se junta si ya existe.
+    // addDoc(userRef, {
+    //   description: description,
+    //   publish_date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+    //   isInSale: true,
+    //   title: state.currentAsset.metadata.name,
+    //   quantity: quantity,
+    //   userID: user.uid,
+    //   cid:
+    //     state.currentAsset.length > 0
+    //       ? state.currentAsset.metadata.image.substring(7)
+    //       : null,
+    // })
+    updateDoc(usersdataRef, {
+      userUID: user.uid,
+      distanceWalked: arrayUnion(distanceWalked),
+      DistanceByVehicle: arrayUnion(distanceByVehicle),
+      WasteProduced: arrayUnion(wasteProduced),
+      NaturalLight: arrayUnion(naturalLight),
+      ShowerTime: arrayUnion(showerTime),
+      Recycled: arrayUnion(recycled),
+      Diet: arrayUnion(diet),
+      dates: arrayUnion(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`),
+    }, 
+    {
+      merge: true
+    })
+      .then(() => {
+        //Limpio el formulario y mando el mensaje de confirmacion
+        setDistanceWalked(0);
+        setDistanceByVehicle(0);
+        setWasteProduced(0);
+        setNaturalLight(0);
+        setShowerTime(0);
+        setRecycled(0);
+        setDiet("Neither");
+
+        //Imprimo los documentos de esa coleccion ::::
+        //const datos = getDataCollection("collections");
+        //console.log(datos);
+        //addCurrentAsset(null);
+
+        toast.success("Asset published successfully! Refreshing the page...", {
+          autoClose: 3000,
+          position: toast.POSITION.TOP_CENTER,
+          theme: "colored",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+
+        toast.error(
+          "Something went wrong when we tried to publish your asset. Please check everything and try again",
+          {
+            autoClose: 8000,
+            position: toast.POSITION.TOP_CENTER,
+            theme: "colored",
+          }
+        );
+      });
+  }
+  
   return (
     <div className="container mt-2">
-      <div className="row">
-        <div className="col-12">
-          <h4>How far did you go today?</h4>
-          <form>
-            <div className="mb-3">
-              <label htmlFor="activity_name" className="form-label">
-                Activity
-              </label>
-              <select id="activity_name" className="form-select">
-                <option>Walking</option>
-                <option>use public transport</option>
-                <option>Lights off in morning</option>
-              </select>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="description" className="form-label">
-                Something to said?
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="description"
-                aria-describedby="description_help"
-              />
-              <div id="description_help" className="form-text">
-                Tell us how do you feel with this achievement!
-              </div>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+      <div className="card card-outline-secondary">
+        <div className="card-header">
+          <h3 className="mb-0">How far did you go today?</h3>
         </div>
-      </div>
-      {/* Here is the new form under */}
-      <div class="card card-outline-secondary">
-        <div class="card-header">
-          <h3 class="mb-0">User Information</h3>
-        </div>
-        <div class="card-body">
-          <form autocomplete="off" class="form" role="form">
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                First name
+        <div className="card-body">
+          <form autoComplete="off" className="form" role="form">
+            {/* <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                What did you do?
               </label>
-              <div class="col-lg-9">
-                <input class="form-control" type="text" value="Jane" />
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Last name
-              </label>
-              <div class="col-lg-9">
-                <input class="form-control" type="text" value="Bishop" />
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Email
-              </label>
-              <div class="col-lg-9">
-                <input
-                  class="form-control"
-                  type="email"
-                  value="email@gmail.com"
-                />
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Company
-              </label>
-              <div class="col-lg-9">
-                <input class="form-control" type="text" value="" />
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Website
-              </label>
-              <div class="col-lg-9">
-                <input class="form-control" type="url" value="" />
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Time Zone
-              </label>
-              <div class="col-lg-9">
-                <select class="form-control" id="user_time_zone" size="0">
-                  <option value="Hawaii">(GMT-10:00) Hawaii</option>
-                  <option value="Alaska">(GMT-09:00) Alaska</option>
+              <div className="col-lg-9">
+                <select className="form-control" id="user_time_zone" size="0">
+                  <option value="Hawaii">Take public transport</option>
+                  <option value="Alaska">Walk 5km</option>
                   <option value="Pacific Time (US &amp; Canada)">
-                    (GMT-08:00) Pacific Time (US &amp; Canada)
-                  </option>
-                  <option value="Arizona">(GMT-07:00) Arizona</option>
-                  <option value="Mountain Time (US &amp; Canada)">
-                    (GMT-07:00) Mountain Time (US &amp; Canada)
-                  </option>
-                  <option
-                    selected="selected"
-                    value="Central Time (US &amp; Canada)"
-                  >
-                    (GMT-06:00) Central Time (US &amp; Canada)
-                  </option>
-                  <option value="Eastern Time (US &amp; Canada)">
-                    (GMT-05:00) Eastern Time (US &amp; Canada)
-                  </option>
-                  <option value="Indiana (East)">
-                    (GMT-05:00) Indiana (East)
+                    Walk 10km
                   </option>
                 </select>
               </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Username
+            </div> */}
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter distance walked (meters)
               </label>
-              <div class="col-lg-9">
-                <input class="form-control" type="text" value="janeuser" />
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={.1} min={0} oninput="validity.valid||(value='');"  onChange={(event) => {
+                    setDistanceWalked(event.target.value);
+                  }} />
               </div>
             </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Password
+            
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter distance traveled by vehicle (kilometers) (Car, bus, train...)
               </label>
-              <div class="col-lg-9">
-                <input
-                  class="form-control"
-                  type="password"
-                  value="11111122333"
-                />
-                <small class="form-text text-muted" id="passwordHelpBlock">
-                  Your password must be 8-20 characters long, contain letters
-                  and numbers, and must not contain spaces, special characters,
-                  or emoji.
-                </small>
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={.1} min={0} oninput="validity.valid||(value='');" onChange={(event) => {
+                    setDistanceByVehicle(event.target.value);
+                  }}/>
               </div>
             </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label">
-                Confirm
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter amount of waste produced
               </label>
-              <div class="col-lg-9">
-                <input
-                  class="form-control"
-                  type="password"
-                  value="11111122333"
-                />
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={.1} min={0} oninput="validity.valid||(value='');" onChange={(event) => {
+                    setWasteProduced(event.target.value);
+                  }}/>
               </div>
             </div>
-            <div class="form-group row">
-              <label class="col-lg-3 col-form-label form-control-label"></label>
-              <div class="col-lg-9">
-                <input class="btn btn-secondary" type="reset" value="Cancel" />
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter hours of natural light
+              </label>
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={.1} min={0} oninput="validity.valid||(value='');" onChange={(event) => {
+                    setNaturalLight(event.target.value);
+                  }}/>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter how many time you use for shower (minutes)
+              </label>
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={.1} min={0} oninput="validity.valid||(value='');" onChange={(event) => {
+                    setShowerTime(event.target.value);
+                  }}/>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Enter amount of waste recycled
+              </label>
+              <div className="col-lg-9">
+                <input className="form-control" type="number" step={1} min={0} oninput="validity.valid||(value='');" onChange={(event) => {
+                    setRecycled(event.target.value);
+                  }}/>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label">
+                Vegetarian/Vegan for the day?
+              </label>
+              <div className="col-lg-9">
+                <select className="form-control" id="user_time_zone" size="0" onChange={(event) => {
+                  console.log(event.target.value);
+                    setDistanceWalked(event.target.value);
+                  }}>
+                  <option value="neither">Neither</option>
+                  <option value="vegetarian">Vegetarian</option>
+                  <option value="vegan">Vegan</option>
+                  
+                </select>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-lg-3 col-form-label form-control-label"></label>
+              <div className="col-lg-9">
+                <input className="btn btn-secondary" type="reset" value="Cancel" />
                 <input
-                  class="btn btn-primary"
+                  className="btn btn-primary"
                   type="button"
-                  value="Save Changes"
+                  value="Submit"
+                  onSubmit={publishActivity}
                 />
               </div>
             </div>
--- no_food_waste_app/src/pages/PostBehaviour.jsx
+++ no_food_waste_app/src/pages/PostBehaviour.jsx
@@ -4,7 +4,14 @@ import ActivityForm from "../components/Activity_form";
 function post_behaviour() {
   return (
     <div>
-      <h3>Share with your circle the sustainable behaviour you have today!</h3>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+          <h3>Share with your circle the sustainable behaviour you have today!</h3>
+          </div>
+        </div>
+      </div>
+      
         <ActivityForm />
     </div>
   )
