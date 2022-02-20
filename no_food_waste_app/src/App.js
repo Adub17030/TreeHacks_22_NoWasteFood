5a693a33bc03b7dc9784f1c2ecbf3e458a1eeb3f
Aditya <aditya.dubey@uconn.edu>
2022-02-19 20:13:06 -0500

    welcom page

--- no_food_waste_app/src/App.js
+++ no_food_waste_app/src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
-import Header from './components/Header';
+import Welcome from "./Welcome";
 import PostBehaviour from "./pages/PostBehaviour";
 
 export default function App() {
   return (
     <div>
-      <Header/>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/activity" element={<PostBehaviour />} />
+          <Route path="/" element={<Welcome />} />
         </Routes>
       </BrowserRouter>
-      {//<Footer />
-}
+      {
+        //<Footer />
+      }
     </div>
   );
 }
---
+++ no_food_waste_app/src/Welcome.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Welcome.css";
+// import LandingText from "../LandingText/LandingText.js";
+// import LandingPhoto from "../LandingPhoto/LandingPhoto";
+// import Navbar from './Components/Navbar/Navbar';
+// import { ReactNavbar } from "react-responsive-animate-navbar";
+import Header from "./components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main>HERE</main>
+    </div>
+  );
+};
+
+export default Home;
